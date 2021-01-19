26577a78e49beaf13b67c0e30426c44bd2b35af8
jaeel29 <elferkhjaber@gmail.com>
2021-01-19 19:43:05 +0100

    create-custom-hook

---
+++ src/components/BlogList/BlogList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const BlogList = ({ blogs, title }) => {
+  return (
+    <div className="blogList">
+      <h2>{title}</h2>
+      {blogs.map((blog) => {
+        return (
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BlogList;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,48 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import BlogList from '../BlogList/BlogList';
+import useFetch from '../useFetch/useFetch';
 import './style.css';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: 'The title of the book 1',
-      body: 'Lorem ipsum ...',
-      author: 'ahmed',
-      id: 1,
-    },
-    {
-      title: 'The title of the book 2',
-      body: 'Lorem ipsum ...',
-      author: 'ayoub',
-      id: 2,
-    },
-    {
-      title: 'The title of the book 3',
-      body: 'Lorem ipsum ...',
-      author: 'mohamed',
-      id: 3,
-    },
-    {
-      title: 'The title of the book 4',
-      body: 'Lorem ipsum ...',
-      author: 'mehdi',
-      id: 3,
-    },
-  ]);
+  const { data: blogs, isPending, error } = useFetch(
+    'http://localhost:8000/blogs'
+  );
 
   return (
     <div className="home">
-      <h1>Home Page</h1>
-
-      {blogs.map((blog) => {
-        return (
-          <div className="blog-preview">
-            <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
-          </div>
-        );
-      })}
-
-      <button>Click here</button>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading ...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
